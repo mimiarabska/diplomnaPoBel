a0feb9509d2d82f898d6683c0a3606ed4f01b2f1
mimiarabska <mimi_arabska@abv.bg>
2023-02-15 21:22:07 +0200

    new folder TablesScripts which contains the script for creating the tables and insert into for category table

---
+++ TablesScripts/InsertIntoCategory.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (`CategoryID`,`CategoryTittle`)
+VALUES(1,'Rodnoto i chujdoto'),
+(2,'Minaloto i pametta'),
+(3,'Obshtestvoto i vlastta '),
+(4,'Jivota i smurtta'),
+(5,'Prirodata'),
+(6,'Lubovta'),
+(7,'Vqrata i nadejdata'),
+(8,'Trudut i tvorchestvoto'),
+(9,'Izborut i razdvoenieto')
