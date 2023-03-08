d48b81dc7c8718cd1468aaa0bc3b7e0103d7826c
Ilina234 <kidikova_3@abv.bg>
2023-03-08 14:58:17 -0800

    InsertIntoAuthor

--- TablesScripts/InsertIntoCategory.sql
+++ TablesScripts/InsertIntoCategory.sql
@@ -1,10 +1,12 @@
 INSERT INTO category (`CategoryID`,`CategoryTittle`)
-VALUES(1,'Rodnoto i chujdoto'),
-(2,'Minaloto i pametta'),
-(3,'Obshtestvoto i vlastta '),
-(4,'Jivota i smurtta'),
-(5,'Prirodata'),
-(6,'Lubovta'),
-(7,'Vqrata i nadejdata'),
-(8,'Trudut i tvorchestvoto'),
-(9,'Izborut i razdvoenieto')
+VALUES(1,'Родното и чуждото'),
+(2,'Миналото и паметта'),
+(3,'Обществото и властта '),
+(4,'Животът и смъртта'),
+(5,'Природата'),
+(6,'Любовта'),
+(7,'Вярата и надеждата'),
+(8,'Трудът и творчеството'),
+(9,'Изборът и раздвоението')
+
+
