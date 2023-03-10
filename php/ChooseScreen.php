c2f5e9e8d404cd6ede5b9fa88840ec7493e41f16
mimiarabska <mimi_arabska@abv.bg>
2023-03-10 11:35:40 +0200

    balkanski sindrom

---
+++ php/ChooseScreen.php
@@ -0,0 +1,104 @@
+<!doctype html>
+    <head>
+
+        <title>Literatureworks</title>
+        <link rel = "stylesheet" href="css/design.css">
+        <link rel = "stylesheet" href="css/ChooseScreen.css">
+        <meta charset="UTF-8">
+    </head>
+        
+        <body>
+         
+         <nav class="col-1">
+          <a href="Welcome.html" target="_blank">Начало</a>
+  
+          <a href="avtoriview.html" target="_blank">Автори</a>
+          
+          <a href="DZI.html" target="_blank">Тестове</a>
+          
+          <a href="screenfeedback.html" target=_blank>Обратна връзка</a>
+        
+             <a href="AboutUs.html" target="_blank">За нас</a>
+         
+        
+         </nav>
+          
+        <div class="col-2">
+          
+          <header>
+            
+        <input type="text">
+          
+          <button>Търсене</button>
+                    
+                    
+         </header>
+                    
+        
+            <main class="content">
+          
+
+            <article>
+            <?php
+  require_once "config.php";
+   //Attempt select query execution
+   $sql = "SELECT * FROM category";
+   if($result = mysqli_query($link, $sql ))
+   if(mysqli_num_rows($result) > 0){
+   while($row = mysqli_fetch_array($result)){
+   echo '<a href="category.php?categoryId=' . $row['CategoryID'] . '">';
+   echo "<h2>";
+   echo $row['CategoryTittle'];
+   echo"</h2>";
+   echo "</a>";
+   }
+   
+   //Free result set
+   
+   mysqli_free_result($result);
+   } else{
+   echo "No records matching your query were found.";
+   }else
+   echo "ERROR: Cold not able to execute $sql. " . mysqli_error($link);
+ 
+  ?>
+       
+        <div class="row">
+          <div class="column">
+            <div class="card">
+              <img src="jelezniqtsvetilnik.jfif" alt="jelezniqtsvetilnik" style="width:100%">
+              <div class="container">
+                <h2>"Железният светилник"</h2>
+                <p class="author">Димитър Талев</p>
+                <p>"Железният светилник"е роман написан от Димитър Талев....</p>
+                <p><button class="button">Прочети</button></p>
+              </div>
+            </div>
+          </div>
+        
+          <div class="column">
+            <div class="card">
+              <img src="baiganio.jfif" alt="baiganio" style="width: 100%">
+              <div class="container">
+                <h2>"Бай Ганьо"</h2>
+                <p class="author">Алеко Константинов</p>
+                <p>"Бай Ганьо" е сборник с фейлетони написан от Алеко Константинов.</p>
+                <p><button class="button">Прочети</button></p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="column">
+            <div class="card">
+              <img src="balkanskisindom.jfif" alt="Balkanskisindrom" style="width: 100%">
+              <div class="container">
+                <h2>"Балкански синдром"</h2>
+                <p class="author">Станислав Стратиев</p>
+                <p>"Балкански синдром" е комедия написана от Станислав Стратиев.</p>
+                <p><button class="button">Прочети</button></p>
+            </div>
+        </div>
+      </div>
+    </div>
+
+         
