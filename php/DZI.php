19bbf3efd15c9c6313ff7bd6a861f8ab657f37d6
mimiarabska <mimi_arabska@abv.bg>
2023-04-05 17:51:14 +0300

    dzi

--- php/DZI.php
+++ php/DZI.php
@@ -14,15 +14,15 @@
  
  <nav class="col-1">
 
-  <а href="Index.php" target="_blank">Начало</а>
+  <a href="Index.php" target="_blank">Начало</a>
   
-  <а href="avtoriview.html" target="_blank">Автори</а>
+  <a href="avtoriview.html" target="_blank">Автори</a>
   
-  <а href="DZI.html" target="_blank">Тестове</а>
+  <a href="DZI.html" target="_blank">Тестове</a>
   
-  <а href="screenfeedback.html" target=_blank>Обратна връзка</а>
+  <a href="screenfeedback.html" target=_blank>Обратна връзка</a>
 
-     <а href="AboutUs.html" target="_blank">За нас</а>
+     <a href="AboutUs.html" target="_blank">За нас</a>
  
 
  </nav>
@@ -44,20 +44,22 @@
 <h1>Изпитни варианти и отговори за ДЗИ БЕЛ!</h1>
 <h3>Изтеглете тест:</h3>
     <div class="button-grid">
-  <а href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2014.pdf" download><button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download><button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2016.pdf" download> <button>2008</button></а>
-  <а href="/diplomna/php/dzi/ДДЗИ%20по%20БЕЛ%2029.08.2016.pdf" download><button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2017.pdf" download> <button>2009</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2018.pdf" download><button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2028.08.2018.pdf" download> <button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2019.pdf" download><button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2008.2019.pdf" download><button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИпоБЕЛ01.06.2020.pdf" download><button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2027.08.2020.pdf" download> <button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2021.pdf" download>  <button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2022.pdf" download> <button>2007</button></а>
-  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2025.08.2022.pdf" download> <button>2007</button></а>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2014.pdf" download><button>ДЗИ 21.05.2014</button></a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> <button>ДЗИ 20.05.2015</button></a>
+   <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2027.08.2015.pdf" download> <button>ДЗИ 27.08.2015</button></a>
+   <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2016.pdf" download><button>ДЗИ 18.05.2016</button></a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2029.08.2016.pdf" download> <button>ДЗИ 29.05.2016</button></a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2017.pdf" download><button>ДЗИ 19.08.2017</button></a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2028.08.2017.pdf" download><button>ДЗИ 28.05.2017</button></a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2018.pdf" download><button>ДЗИ 21.05.2018</button></a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2028.08.2018.pdf" download> <button>ДЗИ 28.08.2018</button> </a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2019.pdf" download><button>ДЗИ 21.05.2019</button> </a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2008.2019.pdf" download><button>ДЗИ 20.08.2019</button> </a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИпоБЕЛ01.06.2020.pdf" download><button>ДЗИ 01.06.2020</button> </a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2027.08.2020.pdf" download><button>ДЗИ 27.08.2020</button> </a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2021.pdf" download> <button>ДЗИ 19.05.2021</button> </a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2022.pdf" download> <button>ДЗИ 18.05.2022</button> </a>
+    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2025.08.2022.pdf" download> <button>ДЗИ 25.08.2022</button> </a>
   </div>
 
 
