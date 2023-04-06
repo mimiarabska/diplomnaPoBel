4cadd6e75f00e63e6c6d9beb4c05999301cfe2d3
Ilina234 <kidikova_3@abv.bg>
2023-04-05 17:01:31 -0700

    tva veche mai e posledno

--- php/DZI.php
+++ php/DZI.php
@@ -44,41 +44,20 @@
 <h1>Изпитни варианти и отговори за ДЗИ БЕЛ!</h1>
 <h3>Изтеглете тест:</h3>
     <div class="button-grid">
-<<<<<<< HEAD
-    <button> <а href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2014.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2016.pdf" download>2008</а></button>
-  <button> <а href="/diplomna/php/dzi/ДДЗИ%20по%20БЕЛ%2029.08.2016.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2017.pdf" download>2009</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2018.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2028.08.2018.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2019.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2008.2019.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИпоБЕЛ01.06.2020.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2027.08.2020.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2021.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2022.pdf" download>2007</а></button>
-  <button> <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2025.08.2022.pdf" download>2007</а></button>
-  
-
-=======
-    <a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> <button>2007</button></a>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2008</button>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2009</button>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2010</button>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2011</button>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2012</button>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2013</button>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2014</button>
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2015</button> 
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2016</button> 
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2017</button> 
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2018</button> 
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2019</button> 
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2020</button> 
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2021</button> 
-    <button><a href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download> 2022</button> 
->>>>>>> 57839cead3759f6d30df64dcf00ee00657ed51c5
+  <а href="/diplomnaPoBel/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2014.pdf" download><button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2005.2015.pdf" download><button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2016.pdf" download> <button>2008</button></а>
+  <а href="/diplomna/php/dzi/ДДЗИ%20по%20БЕЛ%2029.08.2016.pdf" download><button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2017.pdf" download> <button>2009</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2018.pdf" download><button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2028.08.2018.pdf" download> <button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2021.05.2019.pdf" download><button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2008.2019.pdf" download><button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИпоБЕЛ01.06.2020.pdf" download><button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2027.08.2020.pdf" download> <button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2019.05.2021.pdf" download>  <button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2018.05.2022.pdf" download> <button>2007</button></а>
+  <а href="/diplomna/php/dzi/ДЗИ%20по%20БЕЛ%2025.08.2022.pdf" download> <button>2007</button></а>
   </div>
 
 
